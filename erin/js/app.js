77aa1c23bb6bfcc5527af8d1228358f151001b65
erinkirchner <kirc0109@umn.edu>
2014-12-03 18:14:08 -0600

    ek

--- erin/js/app.js
+++ erin/js/app.js
@@ -6,22 +6,20 @@ $(document).ready(function() {
 function init() {
 
 	$("#contact").on("click", function() {
-		clearPortfolio();
+		
 		$("#contactContent").fadeIn(1000);
 	});
 	$("#resume").on("click", function() {
-		clearPortfolio();
+		
 		$("#resumeContent").fadeIn(1000);
 	});
 }
 
 function clearBody() {
 	$("a.listMember").on("click", function() {
+		$(".overlay3").fadeOut(1000);
 		$("#contactContent").fadeOut(1000);
 		$("#resumeContent").fadeOut(1000);
 		
 	});
-}
-function clearPortfolio() {
-	$(".overlay3").slideUp(1000);
 }
